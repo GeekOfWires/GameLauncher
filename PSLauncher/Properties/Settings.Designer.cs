51a2b76fac1379952b382b399b8f241f51c22e12
GeekOfWires <wmbowles201@gmail.com>
2019-04-13 23:19:45 -0400

    Update project configuration and Settings to new default IP 51200.

--- PSLauncher/Properties/Settings.Designer.cs
+++ PSLauncher/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PSLauncher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -99,7 +99,7 @@ namespace PSLauncher.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
             "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring>PSForever,play.psforever.net,51000</string>\r\n</ArrayOfString>")]
+            "tring>PSForever,play.psforever.net,51200</string>\r\n</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection ServerList {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["ServerList"]));
